fa2e1e33c15a30ceb82fce2b8beb7917278093d9
MZC01-SSILOOK <mzc01-ssilook@MZC01-SSILOOK.local>
2026-03-20 08:17:39 +0900

    fix: router path

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,27 +5,19 @@ import Portfolio from '@/pages/Portfolio'
 const router = createBrowserRouter([
     {
       path: '/',
-      element: <DefaultLayout />
-    },
-    {
       element: <DefaultLayout />,
-      // errorElement: <ErrorPageFull />,
       children: [
         {
 					index: true,
 					element: <Portfolio />
 				},
         {
-					path: '/ssilook_portfolio/Portfolio',
+					path: 'Portfolio',
 					element: <Portfolio />
 				}
       ]
     },
-    /* {
-      path: '*',
-      element: <NotFound />
-    } */
-  ])
+  ], { basename: '/ssilook_portfolio' })
   
   export default function Router () {
     return <RouterProvider router={router} />
